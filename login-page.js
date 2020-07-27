01c1eaa54f4ea196910167ae34ff6198dbe694ae
maxstothart2020 <61262211+maxstothart2020@users.noreply.github.com>
2020-07-28 10:35:59 +1200

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -7,7 +7,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "Max" && password === "memoryallwaysprospers") {
+    if (username === "Max" && password === "memory allways prospers") {
         window.location.href = 'https://google.com';
     } else {
         loginErrorMsg.style.opacity = 1;
