775b5215e9eef57fbca7b198dec0667c08bde8e0
maxstothart2020 <61262211+maxstothart2020@users.noreply.github.com>
2020-07-28 15:30:32 +1200

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -7,7 +7,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "Max" && password === "memory allways prospers") {
+    if (username === "max and jadon" && password === "are the best") {
         window.location.href = 'https://maxstothart2020.github.io/secret-fence/css/font/kill/q/w/e/r/t/y/u/i/o/p/a/s/d/f/g/h/j/k/l/z/x/c/v/b/n/m/max.html';
     } else {
         loginErrorMsg.style.opacity = 1;
