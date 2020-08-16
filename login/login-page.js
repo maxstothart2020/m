a9cd7f7733fb2c8a434172ee2e72925df7bacd48
maxstothart2020 <61262211+maxstothart2020@users.noreply.github.com>
2020-08-17 11:13:52 +1200

    Update login-page.js

--- login/login-page.js
+++ login/login-page.js
@@ -5,9 +5,9 @@ const loginErrorMsg = document.getElementById("login-error-msg");
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
-    const password = Request.UserHostAddress;
+    const password = loginForm.password.value;
 
-    if (username === "Max" && password === "210.55.77.175") {
+    if (username === "Max" && password === "Memory allways prospers") {
         window.location.href = 'https://maxstothart2020.github.io/secret-fence/css/font/kill/q/w/e/r/t/y/u/i/o/p/a/s/d/f/g/h/j/k/l/z/x/c/v/b/n/m/max.htm';
     }else if (username === "jadon" && password === "let there be light") {
         window.location.href = 'https://maxstothart2020.github.io/secret-fence/css/font/kill/q/w/e/r/t/y/u/i/o/p/a/s/d/f/g/h/j/k/l/z/x/c/v/b/n/m/jadon.htm';
